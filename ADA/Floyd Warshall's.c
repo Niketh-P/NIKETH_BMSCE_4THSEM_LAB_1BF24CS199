b9d0430cf9404f63bdd6a93ecbd16aa880074788
Niketh-P <nikethp.cs24@bmsce.ac.in>
2026-04-22 14:45:34 +0530

    Create Floyd Warshall's.c

---
+++ ADA/Floyd Warshall's.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#define MAX 100
+#define INF 99999
+
+void printSolution(int dist[MAX][MAX], int V) {
+    printf("\nShortest distance matrix:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%3d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void floydWarshall(int graph[MAX][MAX], int V) {
+    int dist[MAX][MAX];
+
+    // Initialize distance matrix
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Floyd's algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    printSolution(dist, V);
+}
+
+int main() {
+    int V; 
+    int graph[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
+
+    printf("Enter the adjacency matrix:\n");
+    printf("(Use %d to represent INF / no edge)\n", INF);
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    floydWarshall(graph, V);
+
+    return 0;
+}
